ac95928556dbb68539f97ec0d324acd3424b6e6b
Lizochek4874 <babenkoliza04@gmail.com>
2021-10-20 21:21:44 +0300

    message1

--- Source.cpp
+++ Source.cpp
@@ -1,34 +1,63 @@
 #include <iostream>
 #include <string>
+#include"nlohmann/json.hpp"
+#include<vector>
 using namespace std;
+
 
 class Human {
 private:
-
 	string name;
 	int age;
-
+	float weight;
 public:
-
+	void SetName(string name) {
+		this->name = name;
+	}
+	void SetAge(int age) {
+		this->age = age;
+	}
+	void SetWeight(float weight) {
+		this->weight = weight;
+	}
 	string GetName() {
 		return name;
 	}
 	int GetAge() {
 		return age;
 	}
-
-	void SetName(string newName) {
-		name = newName;
+	float GetWeight() {
+		return weight;
 	}
-	void SetAge(int newAge) {
-		age = newAge;
-	}
-
-	void Print() {
+	virtual void Print() {
 		cout << "Name: " << name << endl;
 		cout << "Age: " << age << endl;
+		cout << "Weight: " << weight << endl;
 	}
 };
+class Student :Human {
+	vector<int> marks;
+	string NameGryp;
+public:
+	Student() : Student(" ", 0, {}," ") {
+
+	}
+	Student(string name, int age, vector<int> marks,string NameGryp) :name(name), age(age), marks(marks),NameGryp(NameGryp) {
+
+	}
+	int Average() {
+		int sum = 0;
+		for (int i = 0; i < marks.size(); i++) {
+			sum += marks[i];
+			cout << sum << endl;
+		}
+		return sum / marks.size();
+	}
+};
+/*[21:03] Кальницкий Никита
+Human operator +(const Human& other)*/
+
+
 
 
 
